6c1af065e1f9c83a5e8733a0df72c17393b5725c
leoniddemanenko <demyanenko.leoni@gmail.com>
2023-06-01 19:04:00 +0300

    add validation to add todo page

--- lib/localisation.dart
+++ lib/localisation.dart
@@ -1,10 +1,10 @@
-//as we don't need multi language support here let's keep all localisation strings in one place anyway
+//as we don't need multi language support here let's at least keep all localisation strings in one place
 class AppLocalisation {
   static const addTodoPageTitle = "Add new TODO";
   static const addTodoPageTextFieldHint = "Enter new TODO";
   static const addTodoPageMainButtonTitle = "Add TODO";
+  static const addTodoPageTextFieldValidationError = "Can't create an empty todo";
 
   static const todoListPageTitle = "Todo items";
-  static const todoListPageErrorText =  "Issue with fetching todo items appeared. Please contact the administrator";
-
+  static const todoListPageErrorText = "Issue with fetching todo items appeared. Please contact the administrator";
 }
--- lib/ui/page/add_todo_page.dart
+++ lib/ui/page/add_todo_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -12,32 +10,47 @@ class AddTodoPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    // a way to use a text controller and sored variable with flutter hooks avoiding Statefull widget, make the code more easily reusable and allows to keep const widget
+    final textController = useTextEditingController();
+    final formKey = useMemoized(GlobalKey<FormState>.new, [key]);
 
-    var textController = useTextEditingController();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(AppLocalisation.addTodoPageTitle),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(children: [
-        TextField(
-          autofocus: true,
-          decoration: const InputDecoration(
-            hintText: AppLocalisation.addTodoPageTextFieldHint
-          ),
-          controller: textController,
+        appBar: AppBar(
+          title: const Text(AppLocalisation.addTodoPageTitle),
         ),
-        const Spacer(),
-        ElevatedButton(
-            onPressed: (){
-              var text = textController.text;
-              Provider.of<TodoState>(context, listen: false).addNewTodo(text: text);
-              context.pop();
-        }, child: const Text( AppLocalisation.addTodoPageMainButtonTitle)),
-        const SizedBox(height: 32,),
-      ],),
-      )
-    );
+        body: Form(
+          key: formKey,
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value?.isEmpty == true) {
+                      return AppLocalisation.addTodoPageTextFieldValidationError;
+                    }
+                    return null;
+                  },
+                  autofocus: true,
+                  decoration: const InputDecoration(hintText: AppLocalisation.addTodoPageTextFieldHint),
+                  controller: textController,
+                ),
+                const Spacer(),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState?.validate() == true) {
+                        var text = textController.text;
+                        Provider.of<TodoState>(context, listen: false).addNewTodo(text: text);
+                        context.pop();
+                      }
+                    },
+                    child: const Text(AppLocalisation.addTodoPageMainButtonTitle)),
+                const SizedBox(
+                  height: 32,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
